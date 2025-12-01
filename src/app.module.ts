2eb28d21c2cc4e891be2675cd587b44c92b73721
arturious9090 <sefer07artur@gmail.com>
2025-12-01 11:18:11 +0100

    prisma module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
