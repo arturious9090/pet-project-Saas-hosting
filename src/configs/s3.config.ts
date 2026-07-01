4b6003ffc1a1806746651ce6b4899783ddd3f42a
arturious9090 <sefer07artur@gmail.com>
2026-07-01 13:40:44 +0200

    Update s3.config.ts

--- src/configs/s3.config.ts
+++ src/configs/s3.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs('s3', () => ({
     secretKey: process.env.AWS_SECRET_ACCESS_KEY,
-    keyID: process.env.AWS_ACCESS_KEY_ID,
+    keyId: process.env.AWS_ACCESS_KEY_ID,
     region: process.env.AWS_REGION,
     endpoint: process.env.AWS_ENDPOIN,
     bucket: process.env.AWS_BUCKET
