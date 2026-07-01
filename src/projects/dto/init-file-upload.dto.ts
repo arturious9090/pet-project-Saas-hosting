6dbe25b4b920de06ca6b717c64cab2190b4f5d9f
arturious9090 <sefer07artur@gmail.com>
2026-07-01 13:45:34 +0200

    Update init-file-upload.dto.ts

--- src/projects/dto/init-file-upload.dto.ts
+++ src/projects/dto/init-file-upload.dto.ts
@@ -1,5 +1,6 @@
-import { MimeType } from "@prisma/client";
-import { IsEnum, IsNumber, IsString, Length, Matches } from "class-validator";
+import { IsIn, IsNumber, IsString, Length, Matches } from "class-validator";
+import { MIME_TYPE_BY_VALUE } from "src/common/types/mime-types-by-value";
+import { MimeTypeValue } from "src/common/types/mime-types-value";
 
 
 export class InitFileUploadDto {
@@ -7,8 +8,8 @@ export class InitFileUploadDto {
     fileName!: string;
     @IsNumber()
     size!: number;
-    @IsEnum(MimeType)
-    mimeType!: MimeType;
+    @IsIn(Object.keys(MIME_TYPE_BY_VALUE))
+    mimeType!: MimeTypeValue;
     @IsString()
     @Length(1, 1024)
     @Matches(/^[a-zA-Z0-9/_\-.\s]+$/, {
